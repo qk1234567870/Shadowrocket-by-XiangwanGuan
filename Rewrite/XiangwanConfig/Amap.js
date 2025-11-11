f5ba6b0fb92cceb5c853c33d57fd07bba7e4f889
向晚 <xiangwan.guan@gmail.com>
2025-11-11 15:41:28 +0800

    融合模块：适配2.2.73(2904)

--- Rewrite/XiangwanConfig/Amap.js
+++ Rewrite/XiangwanConfig/Amap.js
@@ -3,7 +3,7 @@
 # 高德地图应用净化；
 # 原作者：@iKeLee，@RuCu6，由向晚重写维护；
 
-# 更新时间: 20250926
+# 更新时间: 20251111
 # 规则链接: https://raw.githubusercontent.com/XiangwanGuan/Shadowrocket/main/Rewrite/XiangwanConfig/Amap.js
 
 [filter_local]
