90a77b0f565731fe4c9ed96c5032b5b4a99049b4
向晚 <xiangwan.guan@gmail.com>
2025-11-12 09:07:46 +0800

    融合模块：修复 extended-matching 和 pre-matching 的配置错误；

--- Rewrite/XiangwanConfig/Amap.js
+++ Rewrite/XiangwanConfig/Amap.js
@@ -3,11 +3,11 @@
 # 高德地图应用净化；
 # 原作者：@iKeLee，@RuCu6，由向晚重写维护；
 
-# 更新时间: 20251111
+# 更新时间: 20251112
 # 规则链接: https://raw.githubusercontent.com/XiangwanGuan/Shadowrocket/main/Rewrite/XiangwanConfig/Amap.js
 
 [filter_local]
-AND, ((URL-REGEX,"^https?:\/\/.+\/amdc\/mobileDispatch"),extended-matching,(USER-AGENT,"AMapiPhone*")), REJECT
+AND, ((URL-REGEX,"^https?:\/\/.+\/amdc\/mobileDispatch",extended-matching),(USER-AGENT,"AMapiPhone*")), REJECT
 DOMAIN, amap-aos-info-nogw.amap.com, REJECT, extended-matching, pre-matching
 DOMAIN, free-aos-cdn-image.amap.com, REJECT, extended-matching, pre-matching
 DOMAIN-SUFFIX, v.smtcdns.com, REJECT, extended-matching, pre-matching
